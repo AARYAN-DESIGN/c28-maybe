51d26ccd706ab69b8b198d082319dbc35a4ae0ef
AARYAN-DESIGN <73704561+AARYAN-DESIGN@users.noreply.github.com>
2021-08-19 21:04:40 +0530

    Add files via upload

---
+++ js/launcher.js
@@ -0,0 +1,31 @@
+class launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.001,
+            length: 100
+        }
+        this.bodyA=bodyA;
+        this.pointB = pointB
+        this.launcher = Constraint.create(options);
+        World.add(world, this.launcher);
+    }
+    attach(body){
+        this.launcher.bodyA = body;
+    }
+    
+    fly(){
+        this.launcher.bodyA = null;
+    }
+display(){
+    if(this.launcher.bodyA){
+ var   pointA=this.bodyA.position;
+ var   pointB=this.pointB;
+ strokeWeight(2);
+    line(pointA.x,pointA.y,pointB.x,pointB.y);
+}
+    
+    }
+    
+}
